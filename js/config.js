98f4ea03dd34ae3b7b77665a447d9aebfc2f3b7b
Yusuf Zubair <yusuf.z@hotmail.co.uk>
2020-04-01 18:41:52 +0100

    add api gateway feature

--- js/config.js
+++ js/config.js
@@ -2,5 +2,6 @@ var configConstants = {
     auth0: {
         domain: 'ENTER_AUTH0_DOMAIN_HERE',
         clientId: 'ENTER_AUTH_CLIENT_ID_HERE'
-    }
+    },
+    apiBaseUrl: 'ENTER_API_BASE_URL_HERE'
 };
